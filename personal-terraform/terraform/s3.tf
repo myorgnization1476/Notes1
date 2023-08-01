cdd8d3dbb10fc41e5ae5c6424c539c5aa0642c02
patilswati98 <126654370+patilswati98@users.noreply.github.com>
2023-08-01 16:57:14 +0530

    Create s3.tf

---
+++ personal-terraform/terraform/s3.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+#backend s3
+terraform {
+  backend "s3" {
+    bucket = "s3bucketobject"
+    key = "terraform/statefiles"
+    region = "ap-south-1"
+  }
+}
